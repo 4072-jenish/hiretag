8ecad006e2bac29fd93892d1a363579b761a6dbc
4072-jenish <150036720+4072-jenish@users.noreply.github.com>
2024-02-06 11:19:25 +0530

    h1.cpp

--- h1.cpp
+++ h1.cpp
@@ -1,3 +1,10 @@
+
+/*
+Q1. WAP that defines a shape class with a constructor that gives value to width and height. 
+The define two sub-classes triangle and rectangle, that calculate the area of the shape area (). 
+In the main, define two variables a triangle and a rectangle and then call the area() function in this two varibles
+*/
+
 #include<iostream>
 
 using namespace std;
