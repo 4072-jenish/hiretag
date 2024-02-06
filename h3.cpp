72de3551a95c15b71b8d99c6cc2627e2301d1e07
4072-jenish <150036720+4072-jenish@users.noreply.github.com>
2024-02-06 11:20:43 +0530

    h3.cpp

--- h3.cpp
+++ h3.cpp
@@ -1,3 +1,7 @@
+/*
+Q3. WAP with a mother class animal. Inside it define a name and an age variables, and set_value() function. 
+Then create two bases variables Zebra and Dolphin which write a message telling the age, the name and giving some extra information (e.g. place of origin).
+*/
 #include<iostream>
 
 using namespace std;
